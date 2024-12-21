47681e659e95534d333cf2f8cbbe9093a7a71e9b
RateteApple <105982649+RateteApple@users.noreply.github.com>
2024-12-22 00:36:07 +0900

    不足していたコマンドの追加

--- src/discord_bot/bot.py
+++ src/discord_bot/bot.py
@@ -19,17 +19,33 @@ class DiscordBot(discord.Client):
         async def summarize(interaction: discord.Interaction):
             await self.command_handler.handle_summary(interaction)
 
-        @self.tree.command(name="silent要約", description="要約結果をあなただけにお届け")
-        async def silent_summarize(interaction: discord.Interaction):
-            await self.command_handler.handle_summary(interaction, silent=True)
-
         @self.tree.command(name="mvp", description="24時間のMVP")
         async def mvp(interaction: discord.Interaction):
             await self.command_handler.handle_mvp(interaction)
 
+        @self.tree.command(name="今北産業", description="過去の100投稿を3行にまとめる")
+        async def imakita(interaction: discord.Interaction):
+            await self.command_handler.handle_imakita(interaction)
+
+        @self.tree.command(name="silent要約", description="要約結果をあなただけにお届け")
+        async def silent_summarize(interaction: discord.Interaction):
+            await self.command_handler.handle_summary(interaction, silent=True)
+
         @self.tree.command(name="silent_mvp", description="mvpをあなただけにお届け")
         async def silent_mvp(interaction: discord.Interaction):
             await self.command_handler.handle_mvp(interaction, silent=True)
+
+        @self.tree.command(name="silent今北産業", description="過去の100投稿を3行にまとめてあなただけにお届け")
+        async def silent_imakita(interaction: discord.Interaction):
+            await self.command_handler.handle_imakita(interaction, silent=True)
+
+        @self.tree.command(name="vc参加", description="BOTをVCに参加させます")
+        async def join(interaction: discord.Interaction):
+            await self.command_handler.handle_voice_join(interaction)
+
+        @self.tree.command(name="vc退出", description="BOTをVCから退出させます")
+        async def leave(interaction: discord.Interaction):
+            await self.command_handler.handle_voice_leave(interaction)
 
         @self.tree.command(name="性格分析mbti", description="過去の投稿から性格を分析")
         async def mbti(interaction: discord.Interaction):
@@ -47,13 +63,13 @@ class DiscordBot(discord.Client):
         async def silent_big5(interaction: discord.Interaction):
             await self.command_handler.handle_personality_analysis(interaction, "BIG5", silent=True)
 
-        @self.tree.command(name="vc参加", description="BOTをVCに参加させます")
-        async def join(interaction: discord.Interaction):
-            await self.command_handler.handle_voice_join(interaction)
+        @self.tree.command(name="年齢推定", description="過去の投稿から年齢を推定")
+        async def age_guess(interaction: discord.Interaction):
+            await self.command_handler.handle_age_estimation(interaction)
 
-        @self.tree.command(name="vc退出", description="BOTをVCから退出させます")
-        async def leave(interaction: discord.Interaction):
-            await self.command_handler.handle_voice_leave(interaction)
+        @self.tree.command(name="silent年齢推定", description="過去の投稿から年齢を推定して、あなただけにお届け")
+        async def silent_age_guess(interaction: discord.Interaction):
+            await self.command_handler.handle_age_estimation(interaction, silent=True)
 
     async def setup_hook(self):
         await self.tree.sync()
